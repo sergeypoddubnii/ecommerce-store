3aa992ca33cd31e4810ae30c429a8e827a0e1041
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-06-18 22:10:45 +0300

    add nav

---
+++ app/components/Nav.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import {Session} from "next-auth";
+import {signIn} from "next-auth/react";
+import Image from "next/image";
+
+export default function Nav({user}:Session){
+    return (
+        <nav className='flex justify-between items-center'>
+            <h1>some navigation</h1>
+            <ul>
+                {!user && (
+                    <li className={'bg-teal-600 text-white py-2 px-4 rounded-md'}>
+                        <button onClick={() => signIn()}>signIn</button>
+                    </li>
+                )}
+                {user && (
+                    <>
+                        <li>
+                            <Image
+                                className='rounded-full'
+                                src={user?.image as string}
+                                alt={user?.name as string}
+                                height={48}
+                                width={48}
+                            />
+                        </li>
+                    </>
+                )}
+            </ul>
+        </nav>
+    )
+};
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,11 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export default NextAuth({
+export const authOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -13,4 +13,6 @@ export default NextAuth({
         }),
     ],
     adapter: PrismaAdapter(prisma),
-})
+};
+
+export default NextAuth(authOptions);
